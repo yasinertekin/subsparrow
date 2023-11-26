7150db292678f5d7cb0d0b09ea56c1a3de3e770c
yasinertekin <yasinertekinn@outlook.com>
2023-11-26 22:56:27 +0300

    Add SubMonthList widget to sub_detail_view.dart

---
+++ lib/feature/sub_detail/view/widget/sub_month_list.dart
@@ -0,0 +1,49 @@
+part of '../sub_detail_view.dart';
+
+final class _SubMonthList extends StatelessWidget {
+  const _SubMonthList({
+    required this.subDetailNotifier,
+    required this.pageController,
+  });
+
+  final SubDetailNotifier subDetailNotifier;
+  final PageController pageController;
+
+  String _formatEnumName(SubLength enumValue) {
+    final enumName = enumValue.toString().split('.').last;
+    final words = enumName.split(RegExp('(?=[A-Z])'));
+
+    for (var i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+    }
+
+    return words.join(' ');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: SubLength.values.length,
+      itemBuilder: (BuildContext context, int index) {
+        final currentSubLength = SubLength.values[index];
+
+        return ListTile(
+          title: Text(
+            _formatEnumName(currentSubLength),
+          ),
+          onTap: () async {
+            subDetailNotifier.monthCount = subDetailNotifier.selectedDate.add(
+              Duration(
+                days: getSubscriptionDays(currentSubLength),
+              ),
+            );
+            await pageController.nextPage(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
+          },
+        );
+      },
+    );
+  }
+}
