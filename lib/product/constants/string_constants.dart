9832b57d25fca0543a449455380864672d87a0c3
yasinertekin <yasinertekinn@outlook.com>
2023-11-30 14:44:57 +0300

    Add string constants for user authentication and home view

---
+++ lib/product/constants/string_constants.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+/// StringConstants
+@immutable
+final class StringConstants {
+  const StringConstants._();
+
+  /// AuthView
+  static const String wrongMessage = 'Something webt wrong:';
+
+  /// AuthView AuthInit
+  static const String welcomeSubSparrow = "SubSparrow'a Hoş Geldiniz";
+
+  /// AuthView Snackbar
+  static const String snackbarMessage = 'Email veya şifre boş olamaz';
+
+  /// Auth AppBar
+  static const String authAppBar = 'User Authentication';
+
+  /// AuthView Snackbar Label
+  static const String snackbarLabel = 'Kapat';
+
+  /// AuthView  EmailTextField
+  static const String email = 'Email';
+
+  /// AuthView PasswordTextField
+  static const String password = 'Şifre';
+
+  /// AuthView ForgotPasswordTextButton
+  static const String forgotPassword = 'Şifremi Unuttum';
+
+  /// AuthView Login Button
+  static const String login = 'Giriş Yap';
+
+  /// AuthView RegisterTextButton
+
+  static const String register = 'Hesap oluştur';
+
+  /// AuthView RegisterView
+  static const String registerQuestion = 'Hesabınız yok mu?';
+
+  /// HomeView
+  static const String mySubscriptions = 'Aboneliklerim';
+
+  /// HomeSubListCard
+  static const String getBack = 'Geri Al';
+
+  /// HomeAppBar
+  static const String welcome = 'Hoş Geldiniz';
+
+  /// HomeTotalPriceCard
+  static const String total = 'Toplam';
+}
