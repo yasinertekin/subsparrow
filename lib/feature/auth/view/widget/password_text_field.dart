e296538935c78d1b91169629e4234f5e6760df57
yasinertekin <yasinertekinn@outlook.com>
2023-11-30 14:43:21 +0300

    Refactor password text field widget to use a notifier

--- lib/feature/auth/view/widget/password_text_field.dart
+++ lib/feature/auth/view/widget/password_text_field.dart
@@ -1,6 +1,6 @@
 part of '../auth_view.dart';
 
-final class _PasswordTextField extends StatefulWidget {
+final class _PasswordTextField extends StatelessWidget {
   const _PasswordTextField({
     required this.passwordController,
   });
@@ -8,35 +8,54 @@ final class _PasswordTextField extends StatefulWidget {
   final TextEditingController passwordController;
 
   @override
-  State<_PasswordTextField> createState() => _PasswordTextFieldState();
-}
-
-class _PasswordTextFieldState extends State<_PasswordTextField> {
-  bool _obscureText = false;
-
-  @override
   Widget build(BuildContext context) {
+    final _PasswordTextFieldNotifier notifier = _PasswordTextFieldNotifier();
+
     return Padding(
       padding: context.padding.low,
-      child: TextField(
-        textInputAction: TextInputAction.done,
-        controller: widget.passwordController,
-        decoration: InputDecoration(
-          labelText: 'Password',
-          suffixIcon: IconButton(
-            icon: Icon(
-              _obscureText ? Icons.visibility_off : Icons.visibility,
+      child: ListenableBuilder(
+        listenable: notifier,
+        builder: (BuildContext context, Widget? child) => TextField(
+          textInputAction: TextInputAction.done,
+          controller: passwordController,
+          decoration: InputDecoration(
+            labelText: StringConstants.password,
+            suffixIcon: IconButton(
+              icon: Icon(
+                notifier.obscureText ? Icons.visibility_off : Icons.visibility,
+              ),
+              onPressed: notifier.changeObscureText,
             ),
-            onPressed: () {
-              setState(() {
-                _obscureText = !_obscureText;
-              });
-            },
           ),
+          obscureText: notifier.obscureText,
+          keyboardType: TextInputType.visiblePassword,
         ),
-        obscureText: _obscureText, // Use the boolean here
-        keyboardType: TextInputType.visiblePassword,
       ),
     );
+  }
+}
+
+final class _PasswordTextFieldNotifier extends ChangeNotifier {
+  /// PasswordTextFieldNotifier constructor
+  _PasswordTextFieldNotifier() {
+    passwordController = TextEditingController();
+  }
+
+  /// passwordController
+  late TextEditingController passwordController;
+
+  bool obscureText = false;
+
+  /// dispose
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  /// changeObscureText
+  void changeObscureText() {
+    obscureText = !obscureText;
+    notifyListeners();
   }
 }
