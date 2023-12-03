99ca7b029434b7edf785cab4b18bef7721fd2028
yasinertekin <yasinertekinn@outlook.com>
2023-12-03 12:12:30 +0300

    Fix setKeyboardOpen method parameter in AuthInitNotifier

--- lib/feature/auth/view_model/auth_init_notifier.dart
+++ lib/feature/auth/view_model/auth_init_notifier.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 /// AuthInitNotifier
-final class AuthInitNotifier extends ChangeNotifier {
+class AuthInitNotifier extends ChangeNotifier {
   /// Keyboard open
   bool keyboardOpen = false;
 
   /// Set keyboard open
-  void setKeyboardOpen(bool value) {
+  void setKeyboardOpen({required bool value}) {
     keyboardOpen = value;
     notifyListeners();
   }
