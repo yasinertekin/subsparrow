432ec08af0480fd7dfd3fb22ce7fba9df908d691
yasinertekin <yasinertekinn@outlook.com>
2023-12-03 11:50:40 +0300

    Refactor home view widgets

---
+++ lib/feature/home/view/widget/home_subscription_card.dart
@@ -0,0 +1,43 @@
+part of '../home_view.dart';
+
+final class _SubscriptionCard extends StatelessWidget {
+  const _SubscriptionCard({
+    required this.index,
+    required this.users,
+    super.key,
+  });
+  final int index;
+  final Users? users;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: context.padding.low,
+      child: Card(
+        child: ListTile(
+          leading: CustomNetworkImage(
+            boxFit: BoxFit.fill,
+            size: const Size(50, 50),
+            emptyWidget: const Icon(
+              Icons.error,
+            ),
+            loadingWidget: const CircularProgressIndicator(),
+            imageUrl: users?.subscriptions[index].icon,
+          ),
+          title: Text(
+            users?.subscriptions[index].platformName.toString() ?? '',
+            style: context.general.textTheme.bodyLarge?.copyWith(),
+          ),
+          subtitle: Text(
+            users?.subscriptions[index].name ?? '',
+            style: context.general.textTheme.bodySmall?.copyWith(),
+          ),
+          trailing: Text(
+            '${users?.subscriptions[index].price} ₺',
+            style: context.general.textTheme.bodyLarge?.copyWith(),
+          ),
+        ),
+      ),
+    );
+  }
+}
