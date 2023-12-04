b55721264548f323352e3764f3e2668324d36f22
Yasin Ertekin <yasinertekinn@outlook.com>
2023-12-04 23:42:40 +0300

    Refactor AuthMixin for authentication functionality

--- lib/feature/auth/view/auth_view.dart
+++ lib/feature/auth/view/auth_view.dart
@@ -1,155 +1,91 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
 import 'package:gen/gen.dart';
-import 'package:gen/src/model/auth/auth.dart';
 import 'package:kartal/kartal.dart';
+import 'package:provider/provider.dart';
 import 'package:subsparrow/feature/auth/view/mixin/auth_mixin.dart';
-import 'package:subsparrow/feature/auth/view_model/auth_init_notifier.dart';
-import 'package:subsparrow/feature/auth/view_model/cubit/auth_cubit.dart';
-import 'package:subsparrow/feature/auth/view_model/state/auth_state.dart';
-import 'package:subsparrow/feature/dashboard/view/dashboard_view.dart';
+import 'package:subsparrow/feature/auth/view_model/custom_auth_provider.dart';
+import 'package:subsparrow/feature/register/view/register_view.dart';
 import 'package:subsparrow/product/constants/string_constants.dart';
 
 part 'widget/email_text_field.dart';
-part 'widget/forgot_password_text_button.dart';
+part 'widget/forgot_my_password.dart';
 part 'widget/login_button.dart';
 part 'widget/password_text_field.dart';
 part 'widget/register_text_button.dart';
 
-/// View for user authentication
+/// The main authentication view widget.
+/// This widget includes a login form with email and password fields.
 final class AuthView extends StatefulWidget {
-  /// Constructor for [AuthView]
-  const AuthView({
-    super.key,
-  });
+  /// [AuthView] default constructor
+  const AuthView({super.key});
 
   @override
-  State<AuthView> createState() => _AuthViewState();
+  // ignore: library_private_types_in_public_api
+  _AuthViewState createState() => _AuthViewState();
 }
 
-class _AuthViewState extends State<AuthView> with AuthMixin {
+final class _AuthViewState extends State<AuthView> with AuthMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          StringConstants.welcomeSubSparrow,
-        ),
-      ),
-      body: BlocBuilder<AuthCubit, AuthState>(
-        builder: (context, state) {
-          switch (state.runtimeType) {
-            case AuthInitial:
-              return _AuthInit(
-                emailController: emailController,
-                passwordController: passwordController,
-              );
-            case AuthLoading:
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            case AuthSuccess:
-              WidgetsBinding.instance.addPostFrameCallback((_) {
-                context.route.navigateToPage(
-                  const DashboardView(),
-                );
-              });
-              return const SizedBox.shrink();
-
-            case AuthFailure:
-              return ListView(
-                children: [
-                  _AuthInit(
-                    emailController: emailController,
-                    passwordController: passwordController,
-                  ),
-                  Text(
-                    '${StringConstants.wrongMessage}${(state as AuthFailure).error}',
-                  ),
-                ],
-              );
-            default:
-              return const SizedBox.shrink();
-          }
-        },
-      ),
-    );
-  }
-}
-
-final class _AuthInit extends StatelessWidget {
-  const _AuthInit({
-    required this.emailController,
-    required this.passwordController,
-  });
-
-  final TextEditingController emailController;
-  final TextEditingController passwordController;
-
-  @override
-  Widget build(BuildContext context) {
-    final authInitNotifier = AuthInitNotifier();
-    return ListenableBuilder(
-      listenable: authInitNotifier,
-      builder: (BuildContext context, Widget? child) => Center(
-        child: SingleChildScrollView(
+      body: SafeArea(
+        child: Padding(
+          padding: context.padding.low,
           child: Column(
-            mainAxisAlignment: authInitNotifier.keyboardOpen ? MainAxisAlignment.spaceEvenly : MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Assets.icons.icAuth.svg(
                 package: 'gen',
-                height:
-                    authInitNotifier.keyboardOpen ? context.sized.dynamicHeight(0.2) : context.sized.dynamicHeight(0.4),
+                height: context.sized.dynamicHeight(
+                  context.general.isKeyBoardOpen == true ? 0.15 : 0.45,
+                ),
               ),
-              _EmailTextField(
-                authInitNotifier: authInitNotifier,
+              _CustomForm(
+                formKey: formKey,
                 emailController: emailController,
-              ),
-              _PasswordTextField(
                 passwordController: passwordController,
               ),
-              if (context.general.isKeyBoardOpen)
-                const SizedBox.shrink()
-              else
-                Column(
-                  children: [
-                    const _ForgotPasswordTextButton(),
-                    _LoginButton(
-                      onPressed: () {
-                        _signInControl(context);
-                      },
-                    ),
-                    const _RegisterTextButton(),
-                  ],
-                ),
+              const _ForgotMyPassword(),
+              _LoginButton(
+                onPressed: () async {
+                  await signInUserAndNavigate(
+                    context.read<CustomAuthProvider>(),
+                    context,
+                  );
+                },
+              ),
+              const _RegisterTextButton(),
             ],
           ),
         ),
       ),
     );
   }
+}
 
-  void _signInControl(BuildContext context) {
-    emailController.text.isNotEmpty && passwordController.text.isNotEmpty
-        ? context.read<AuthCubit>().signIn(
-              UserModel(
-                email: passwordController.text,
-                password: passwordController.text,
-              ),
-              emailController,
-              passwordController,
-            )
-        : ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: const Text(
-                StringConstants.snackbarMessage,
-              ),
-              duration: const Duration(seconds: 2),
-              action: SnackBarAction(
-                label: StringConstants.snackbarLabel,
-                onPressed: () {},
-              ),
-            ),
-          );
+final class _CustomForm extends StatelessWidget {
+  const _CustomForm({
+    required this.formKey,
+    required this.emailController,
+    required this.passwordController,
+  });
+
+  final GlobalKey<FormState> formKey;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          _EmailTextField(emailController: emailController),
+          const Gap(15),
+          _PasswordTextField(passwordController: passwordController),
+        ],
+      ),
+    );
   }
 }
--- lib/feature/auth/view/mixin/auth_mixin.dart
+++ lib/feature/auth/view/mixin/auth_mixin.dart
@@ -1,11 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
 import 'package:subsparrow/feature/auth/view/auth_view.dart';
+import 'package:subsparrow/feature/auth/view_model/custom_auth_provider.dart';
+import 'package:subsparrow/feature/dashboard/view/dashboard_view.dart';
 
-/// Mixin for [AuthView]
+/// A mixin for [AuthView] providing common authentication functionality.
+///
+/// This mixin includes a [formKey] for managing the form state,
+/// [emailController] for handling email input, and
+/// [passwordController] for managing password input.
 mixin AuthMixin on State<AuthView> {
-  /// Controllers for [TextField]s
-  final TextEditingController emailController = TextEditingController();
+  /// A key to uniquely identify and manage the form state.
+  final _formKey = GlobalKey<FormState>();
 
-  /// Controllers for [TextField]s
-  final TextEditingController passwordController = TextEditingController();
+  /// Getter for the form key.
+  GlobalKey<FormState> get formKey => _formKey;
+
+  /// Controller for handling email input in the authentication form.
+  TextEditingController emailController = TextEditingController();
+
+  /// Controller for handling password input in the authentication form.
+  TextEditingController passwordController = TextEditingController();
+
+  /// [signInUserAndNavigate] Function
+  Future<void> signInUserAndNavigate(
+    CustomAuthProvider userRegisterViewModel,
+    BuildContext context,
+  ) async {
+    try {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          duration: Duration(seconds: 1),
+          content: Text('Giriş yapılıyor...'),
+        ),
+      );
+      if (formKey.currentState!.validate()) {
+        await userRegisterViewModel.signIn(
+          emailController.text,
+          passwordController.text,
+        );
+
+        // Registration successful, you can navigate
+        //to the next screen or take any other action.
+
+        navigateToDashboardView(
+          context,
+        );
+      }
+    } catch (error) {
+      // Handle and display the error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('$error'),
+        ),
+      );
+    }
+  }
+
+  /// [navigateToDashboardView] Function
+  void navigateToDashboardView(BuildContext context) {
+    context.route.navigateToPage(const DashboardView());
+  }
 }
