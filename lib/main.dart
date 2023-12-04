58805c1dd22e4b79d0379a2dfb68b238da891bef
Yasin Ertekin <yasinertekinn@outlook.com>
2023-12-04 23:42:25 +0300

    Update dependencies and providers in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import 'package:subsparrow/feature/auth/view/auth_view.dart';
-import 'package:subsparrow/feature/auth/view_model/cubit/auth_cubit.dart';
+import 'package:subsparrow/feature/auth/view_model/custom_auth_provider.dart';
+import 'package:subsparrow/feature/register/view/register_view.dart';
+import 'package:subsparrow/feature/register/view_model/register_view_model.dart';
 import 'package:subsparrow/product/initialize/app_initialize.dart';
 import 'package:subsparrow/product/initialize/theme/custom_dark_theme.dart';
 import 'package:subsparrow/product/service/auth_service.dart';
@@ -9,7 +11,24 @@ import 'package:subsparrow/product/service/auth_service.dart';
 Future<void> main() async {
   await AppInitialize().make();
 
-  runApp(const _MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => CustomAuthProvider(
+            AuthServices(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => PasswordNotifier(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => UserRegisterViewModel(),
+        ),
+      ],
+      child: const _MyApp(),
+    ),
+  );
 }
 
 final class _MyApp extends StatelessWidget {
@@ -21,16 +40,7 @@ final class _MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: CustomDarkTheme().themeData,
-      home: MultiBlocProvider(
-        providers: [
-          BlocProvider<AuthCubit>(
-            create: (context) => AuthCubit(
-              authService: AuthServices(),
-            ),
-          ),
-        ],
-        child: const AuthView(),
-      ),
+      home: const RegisterView(),
     );
   }
 }
