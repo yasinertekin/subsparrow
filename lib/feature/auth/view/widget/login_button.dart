d2ad1587d2636e33cf61a824a853ff6dc10d683d
Yasin Ertekin <yasinertekinn@outlook.com>
2023-12-04 23:42:57 +0300

    Refactor login button widget

--- lib/feature/auth/view/widget/login_button.dart
+++ lib/feature/auth/view/widget/login_button.dart
@@ -1,25 +1,22 @@
 part of '../auth_view.dart';
 
 final class _LoginButton extends StatelessWidget {
-  const _LoginButton({required this.onPressed});
-  final VoidCallback onPressed;
+  const _LoginButton({
+    required this.onPressed,
+  });
+
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    const height = 0.06;
-    const fontSize = 20.0;
     return SizedBox(
       width: double.infinity,
-      height: context.sized.dynamicHeight(height),
       child: ElevatedButton(
-        onPressed: onPressed,
-        child: Text(
-          StringConstants.login,
-          style: context.general.textTheme.titleLarge?.copyWith(
-            fontSize: fontSize,
-            fontWeight: FontWeight.bold,
-          ),
+        style: ElevatedButton.styleFrom(
+          elevation: 6,
         ),
+        onPressed: onPressed,
+        child: const Text('Giriş yap'),
       ),
     );
   }
