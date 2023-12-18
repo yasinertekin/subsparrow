d2f81123775c0ecb4996771aa8255773ab6a2e2e
Yasin Ertekin <yasinertekinn@outlook.com>
2023-12-18 16:39:51 +0300

    Add ProfileEnum for list tile titles

---
+++ lib/product/utility/enum/profile_enum.dart
@@ -0,0 +1,15 @@
+/// The [ProfileEnum] enum class is used to get the title of the list tile.
+enum ProfileEnum {
+  /// The [changeName] enum value is used to get the title of the list tile.
+  changeName,
+
+  /// The [email] enum value is used to get the title of the list tile.
+  email,
+
+  /// The [changeThemeColor] enum value is used
+  /// to get the title of the list tile.
+  changeThemeColor,
+
+  /// The [signOut] enum value is used to get the title of the list tile.
+  signOut,
+}
