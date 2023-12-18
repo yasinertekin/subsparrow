1be680516d577dfaf00934114fd10d55b10d755d
Yasin Ertekin <yasinertekinn@outlook.com>
2023-12-18 16:39:25 +0300

    Remove unused code

--- lib/feature/subscription/view/mixin/subscription_mixin.dart
+++ lib/feature/subscription/view/mixin/subscription_mixin.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gen/src/model/subscription_data/subscription_data.dart';
 import 'package:subsparrow/feature/subscription/view/subscription_view.dart';
 import 'package:subsparrow/feature/subscription/view_model/subscription_view_model.dart';
 
@@ -9,11 +8,4 @@ mixin SubscriptionMixin on State<SubscriptionView> {
 
   /// SubscriptionViewModel
   SubscriptionViewModel get subscriptionViewModel => _subscriptionViewModel;
-
-  /// Add Subscription
-  Future<List<SubscriptionData>> getSubscriptionData() async {
-    final data = await subscriptionViewModel.getSubscriptionData();
-
-    return data;
-  }
 }
