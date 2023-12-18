f5f52d6cc42292b1c17b84c8e687ff4788c58ee2
Yasin Ertekin <yasinertekinn@outlook.com>
2023-12-18 16:40:27 +0300

    Refactor Subscriptions class and add documentation

--- module/gen/lib/src/model/subscriptions/subscriptions.dart
+++ module/gen/lib/src/model/subscriptions/subscriptions.dart
@@ -4,7 +4,10 @@ import 'package:json_annotation/json_annotation.dart';
 part 'subscriptions.g.dart';
 
 @JsonSerializable()
-class Subscriptions with EquatableMixin {
+
+/// [Subscriptions] is a class that holds the data of a subscription.
+final class Subscriptions with EquatableMixin {
+  /// [Subscriptions] is a class that holds the data of a subscription.
   Subscriptions({
     this.name,
     this.price,
@@ -15,27 +18,56 @@ class Subscriptions with EquatableMixin {
     this.status,
   });
 
-  factory Subscriptions.fromJson(Map<String, dynamic> json) => _$SubscriptionsFromJson(json);
+  // [Subscriptions] from a [json].
+  factory Subscriptions.fromJson(Map<String, dynamic> json) => _$SubscriptionsFromJson(
+        json,
+      );
   @JsonKey(name: 'name')
+
+  /// [name] is the name of the subscription.
   final String? name;
+
   @JsonKey(name: 'price')
+
+  /// [price] is the price of the subscription.
   final double? price;
   @JsonKey(name: 'Platform Name')
+
+  /// [platformName] is the platform name of the subscription.
   final String? platformName;
   @JsonKey(name: 'Start Date')
+
+  /// [startDate] is the start date of the subscription.
   final DateTime? startDate;
   @JsonKey(name: 'End Date')
+
+  /// [endDate] is the end date of the subscription.
   final DateTime? endDate;
   @JsonKey(name: 'Icon')
+
+  /// [icon] is the icon of the subscription.
   final String? icon;
   @JsonKey(name: 'Status')
+
+  /// [status] is the status of the subscription.
   final bool? status;
 
+  /// [toJson] is a method that converts a [Subscriptions] into a [json].
   Map<String, dynamic> toJson() => _$SubscriptionsToJson(this);
 
   @override
-  List<Object?> get props => [name, price, platformName, startDate, endDate, icon, status];
+  List<Object?> get props => [
+        name,
+        price,
+        platformName,
+        startDate,
+        endDate,
+        icon,
+        status,
+      ];
 
+  /// [copyWith] is a method that
+  /// copies a [Subscriptions] into a new [Subscriptions].
   Subscriptions copyWith({
     String? name,
     double? price,
